e250209451942d5c04ace5b12e32897ebc58ee30
Anatoly <amosendz@mail.ru>
2017-04-05 19:17:19 +0300

    xml_start

--- stitalizator01/Controllers/ProgramsController.cs
+++ stitalizator01/Controllers/ProgramsController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using stitalizator01.Models;
+using System.IO;
+using System.Xml;
 
 namespace stitalizator01.Controllers
 {
@@ -17,6 +19,7 @@ namespace stitalizator01.Controllers
         // GET: Programs
         public ActionResult Index()
         {
+            getScheduleByDateChannel();
             return View(db.Programs.ToList());
         }
 
@@ -113,6 +116,43 @@ namespace stitalizator01.Controllers
             db.Programs.Remove(program);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+
+        public List<Tuple<string,string,string,string,string,DateTime>> getScheduleByDateChannel (string dateStr = "05.04.2017", string chCodeStr = "10")
+        {
+            List<string> sched = new List<string>();
+            List<Tuple<string, string, string, string, string, DateTime>> schedParsed = new List<Tuple<string, string, string, string, string, DateTime>>();
+            string url = "http://xmltv.s-tv.ru/xchenel.php?xmltv=1&pass=jJoM88wN54&show=2&login=tv4181";
+            WebClient client = new WebClient();            
+
+            string xmlString = client.DownloadString(url);            
+            string[] delim = {"<File>"};
+            sched = xmlString.Split(delim,System.StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string name = "";
+            string efirWeek = "";
+            string channel = "";
+            string channelID = "";
+            string variant = "";
+            DateTime dateTime;
+            
+            foreach(string s in sched)
+            {
+                if (s.IndexOf("<Name>")>=0)
+                {
+                    name = s.Substring(s.IndexOf("<Name>") + 6, s.IndexOf("</Name>") - s.IndexOf("<Name>") - 6);
+                    efirWeek = s.Substring(s.IndexOf("<EfirWeek>") + 10, s.IndexOf("</EfirWeek>") - s.IndexOf("<EfirWeek>") - 10);                    
+                    channel = s.Substring(s.IndexOf("<Channel>") + 9, s.IndexOf("</Channel>") - s.IndexOf("<Channel>") - 9);
+                    channelID = s.Substring(s.IndexOf("<ChannelID>") + 11, s.IndexOf("</ChannelID>") - s.IndexOf("<ChannelID>") - 11);
+                    variant = s.Substring(s.IndexOf("<Variant>") + 9, s.IndexOf("</Variant>") - s.IndexOf("<Variant>") - 9);
+                    dateTime = DateTime.Parse(s.Substring(s.IndexOf("<DateTime>") + 10, s.IndexOf("</DateTime>") - s.IndexOf("<DateTime>") - 10));                    
+                    Tuple<string, string, string, string, string, DateTime> curTuple = new Tuple<string, string, string, string, string, DateTime>(name,efirWeek,channel,channelID,variant,dateTime);
+                    schedParsed.Add(curTuple);
+                }
+            }
+
+            return schedParsed;
         }
 
         protected override void Dispose(bool disposing)
