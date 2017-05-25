95a15cc51f49c0b8b4ebe77e2acb8581f30dcdcf
Anatoly <amosendz@mail.ru>
2017-05-25 14:20:44 +0300

    mail

--- stitalizator01/Global.asax.cs
+++ stitalizator01/Global.asax.cs
@@ -7,14 +7,18 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using stitalizator01.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace stitalizator01
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         public static System.Timers.Timer timer = new System.Timers.Timer(60000); // This will raise the event every one minute.
+        public static System.Timers.Timer timer10 = new System.Timers.Timer(10*60000); 
         public static ApplicationDbContext db = new ApplicationDbContext();
         public static TimeSpan utcMoscowShift = TimeSpan.FromHours(3);
+        private DateTime lastWarningTimeStamp = DateTime.UtcNow-TimeSpan.FromDays(1);
 
         protected void Application_Start()
         {
@@ -28,6 +32,76 @@ namespace stitalizator01
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             timer.Enabled = true;
             timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer10.Enabled = true;
+            timer10.Elapsed += new System.Timers.ElapsedEventHandler(timer10_Elapsed);
+
+
+        }
+
+        private void Contact()
+        {
+            DateTime curTime = DateTime.UtcNow;
+            List<ApplicationUser> users = db.Users.ToList();
+            List<Bet> userBets = new List<Bet>();
+            //var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+            var message = new MailMessage();
+
+            foreach (ApplicationUser user in users)
+            {
+                userBets = db.Bets.Where(b => b.ApplicationUser.UserName == user.UserName & b.Program.TvDate == curTime.Date & b.BetSTIplus == 0).ToList();
+
+                if (user.UserName != "admin")
+                {
+                    if (userBets.Count() > 0)
+                    {
+                        message.To.Add(new MailAddress(user.Email));
+                    }
+                }
+            }
+            if (message.To.Count() > 0)
+            {
+                //message.To.Add(new MailAddress("amosendz@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("stitalizator@gmail.com");  // replace with valid value
+                message.Subject = "Нужно сделать ставки на " + curTime.Date.ToString("dd.MM.yyyy");
+                message.Body = "В далеком светлом будущем здесь будет перечень ставок.";
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "stitalizator@gmail.com",  // replace with valid value
+                        Password = "945549Co"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.Send(message);
+                    //return RedirectToAction("Sent");
+                    //return Content("Sent!");
+                }
+            }
+            else
+            {
+               // return Content("Все молодцы! Отправлять некому!");
+            }
+        }
+
+
+        private void timer10_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime now = DateTime.UtcNow + utcMoscowShift;
+            List<Bet> burningBets = db.Bets.Where(b => b.Program.TimeStart < (now+TimeSpan.FromHours(1))).ToList();
+            if (lastWarningTimeStamp.Date < now.Date)
+            {                
+                if (burningBets.Count > 0)
+                {
+                    Contact();
+                    lastWarningTimeStamp = now;    
+                }
+            }
+
         }
 
         private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
